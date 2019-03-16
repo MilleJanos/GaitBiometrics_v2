b3e1e78d9a49b99a412e4f77643c7c2c97710b5e
Janos MIlle <millejanos31@gmail.com>
2019-03-16 21:27:38 +0200

    Firebase Auth Fragment added.

--- app/src/main/java/ms/sapientia/gaitbiometricsapp/controller/activityes/MainActivity.java
+++ app/src/main/java/ms/sapientia/gaitbiometricsapp/controller/activityes/MainActivity.java
@@ -1,13 +1,14 @@
 package ms.sapientia.gaitbiometricsapp.controller.activityes;
 
-import android.content.Intent;
+import android.content.Context;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toolbar;
+import android.widget.Toast;
 
 import ms.sapientia.gaitbiometricsapp.R;
 import ms.sapientia.gaitbiometricsapp.controller.fragments.FirebaseAuthenticationFragment;
@@ -15,6 +16,7 @@ import ms.sapientia.gaitbiometricsapp.controller.fragments.FirebaseAuthenticatio
 public class MainActivity extends AppCompatActivity {
 
     private final String TAG = "MainActivity";
+    private final Context context = MainActivity.this;
     private Button mExitButton;
     private TextView mTitleTextView;
     private Button mFirebaseAuthButton;
@@ -27,7 +29,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         bindView();
-        setClickListeners();
+        initTexts();
+        bindOnClicks();
 
     }
 
@@ -38,7 +41,13 @@ public class MainActivity extends AppCompatActivity {
         mGaitAuthButton = findViewById(R.id.button_gait_auth);
     }
 
-    private void setClickListeners() {
+    private void initTexts(){
+        mTitleTextView.setText(getString(R.string.main_title));
+        mFirebaseAuthButton.setText(getString(R.string.main_firebase_auth));
+        mGaitAuthButton.setText(getString(R.string.main_gait_auth));
+    }
+
+    private void bindOnClicks() {
         mExitButton.setOnClickListener(exitButtonOnClickListener);
         mFirebaseAuthButton.setOnClickListener(firebaseButtonOnClickListener);
         mGaitAuthButton.setOnClickListener(gaitButtonOnClickListener);
@@ -59,8 +68,14 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onClick(View v) {
             Log.i(TAG,"Firebase Auth button pressed.");
-
-            // TODO: START FRAGMENT WITH RESULT (result: TAGged in state)
+            //Bundle args = new Bundle();
+            //args.putParcelable("my_custom_objects_key", myObject);
+            FirebaseAuthenticationFragment fragment = new FirebaseAuthenticationFragment();
+            //fragment.setArguments(args);
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            transaction.add(R.id.activity_main_container, fragment);
+            transaction.commit();
+            mExitButton.setVisibility(View.INVISIBLE);  // TODO: Why I have to hide it manually? (1)
         }
     };
     //endregion
@@ -71,9 +86,23 @@ public class MainActivity extends AppCompatActivity {
         public void onClick(View v) {
             Log.i(TAG,"Gait auth button pressed.");
 
-            // TODO: START FRAGMENT WITH RESULT (result: TAGged in state)
+            // TODO: START FRAGMENT
+            Toast.makeText(context,"YAP", Toast.LENGTH_SHORT).show();
         }
     };
     //endregion ...
 
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        mExitButton.setVisibility(View.VISIBLE);    // TODO: Why I have to hide it manually? (2)
+        Log.i(TAG,"onResume");
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        Log.i(TAG,"onPause");
+    }
 }
--- app/src/main/java/ms/sapientia/gaitbiometricsapp/controller/fragments/FirebaseAuthenticationFragment.java
+++ app/src/main/java/ms/sapientia/gaitbiometricsapp/controller/fragments/FirebaseAuthenticationFragment.java
@@ -1,6 +1,7 @@
 package ms.sapientia.gaitbiometricsapp.controller.fragments;
 
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -8,12 +9,27 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import ms.sapientia.gaitbiometricsapp.R;
+import ms.sapientia.gaitbiometricsapp.controller.activityes.MainActivity;
 
 public class FirebaseAuthenticationFragment extends Fragment {
 
     private FirebaseAuthenticationViewModel mViewModel;
+    private Button mBackButton;
+    private TextView mTitleTextView;
+    private EditText mFieldEditText0;   // email
+    private EditText mFieldEditText1;   // password
+    private EditText mFieldEditText2;   // password (re-enter)
+    private Button mMainButton;
+    private Button mAdditionalButton0;
+    private Button mAdditionalButton1;
+    private Button mAdditionalButton2;
+
 
     public static FirebaseAuthenticationFragment newInstance() {
         return new FirebaseAuthenticationFragment();
@@ -22,14 +38,112 @@ public class FirebaseAuthenticationFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.firebase_authentication_fragment, container, false);
+        View view = inflater.inflate(R.layout.firebase_authentication_fragment, container, false);
+        bindView(view);
+        bindOnClicks();
+        initTexts();
+
+        return view;
     }
+
+
+
+    private void bindView(View view) {
+        mBackButton = view.findViewById(R.id.button_back);
+        mTitleTextView = view.findViewById(R.id.text_title);
+        mFieldEditText0 = view.findViewById(R.id.edit_text_0);
+        mFieldEditText1 = view.findViewById(R.id.edit_text_1);
+        mFieldEditText2 = view.findViewById(R.id.edit_text_2);
+        mMainButton = view.findViewById(R.id.button_main);
+        mAdditionalButton0 = view.findViewById(R.id.button_additional_0);
+        mAdditionalButton1 = view.findViewById(R.id.button_additional_1);
+        mAdditionalButton2 = view.findViewById(R.id.button_additional_2);
+    }
+
+    private void initTexts() {
+        mTitleTextView.setText(getString(R.string.firebase_auth_login_title));
+        mFieldEditText0.setHint(getString(R.string.firebase_auth_login_field_1));
+        mFieldEditText1.setHint(getString(R.string.firebase_auth_login_field_2));
+        mFieldEditText2.setVisibility(View.GONE);
+        mMainButton.setText(getString(R.string.firebase_auth_login_main_button));
+        mAdditionalButton0.setText(getString(R.string.firebase_auth_login_additional_button_1));
+        mAdditionalButton1.setText(getString(R.string.firebase_auth_login_additional_button_2));
+        mAdditionalButton2.setVisibility(View.GONE);
+    }
+
+    private void bindOnClicks() {
+        mBackButton.setOnClickListener(backButtonOnClickListener);
+        mMainButton.setOnClickListener(loginOnClickListener);
+        mAdditionalButton0.setOnClickListener(gotoRegisterOnClickListener);
+        mAdditionalButton1.setOnClickListener(forgotPasswordOnClickListener);
+    }
+
+    //region View.OnClickListener backButtonOnClickListener
+    private View.OnClickListener backButtonOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            closeFragment();
+        }
+    };
+    //endregion
+
+    //region View.OnClickListener loginOnClickListener
+    private View.OnClickListener loginOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            // TODO: Login
+            Toast.makeText(getActivity(),"login", Toast.LENGTH_SHORT).show();
+
+        }
+    };
+    //endregion
+
+    //region View.OnClickListener registerOnClickListener
+    private View.OnClickListener registerOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            // TODO: Register
+            Toast.makeText(getActivity(),"register main button", Toast.LENGTH_SHORT).show();
+
+        }
+    };
+    //endregion
+
+    //region View.OnClickListener gotoRegisterOnClickListener
+    private View.OnClickListener gotoRegisterOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            // TODO: goto Register (create account small button)
+            Toast.makeText(getActivity(),"go to register", Toast.LENGTH_SHORT).show();
+
+        }
+    };
+    //endregion
+
+    //region View.OnClickListener forgotPasswordOnClickListener
+    private View.OnClickListener forgotPasswordOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            // TODO: forgot password (small button)
+            Toast.makeText(getActivity(),"forgot pass", Toast.LENGTH_SHORT).show();
+
+        }
+    };
+    //endregion
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         mViewModel = ViewModelProviders.of(this).get(FirebaseAuthenticationViewModel.class);
         // TODO: Use the ViewModel
+    }
+
+    private void closeFragment(){
+        getActivity().getSupportFragmentManager().beginTransaction().remove(FirebaseAuthenticationFragment.this).commit();
     }
 
 }
